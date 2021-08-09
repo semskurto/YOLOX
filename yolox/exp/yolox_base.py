82674f54afe7ac9c7e0209453ed1d36d9f5368f8
Şems <31928447+semskurto@users.noreply.github.com>
2021-08-09 19:49:22 +0300

    Update yolox_base.py

--- yolox/exp/yolox_base.py
+++ yolox/exp/yolox_base.py
@@ -62,7 +62,7 @@ class Exp(BaseExp):
         self.nmsthre = 0.65
 
     def get_model(self):
-        from yolox.models import YOLOX, YOLOPAFPN, YOLOXHead
+        from YOLOX.yolox.models import YOLOX, YOLOPAFPN, YOLOXHead
 
         def init_yolo(M):
             for m in M.modules():
